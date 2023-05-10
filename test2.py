f79ecfb7950d95b9c32e0a67075f9eedb6f45d90
456i <timofeymarushko@gmail.com>
2023-05-10 20:49:34 +0300

    Commit

--- test2.py
+++ test2.py
@@ -33,39 +33,39 @@ def make_node():
         STACK[apex] = 1
 
 
-# class Apex:
-#     def __init__(self, dictionary):
-#         self.dict = dictionary
-#         # self.trial_dict = self.dict.copy()
-#         self.nodes = None
-#         self.lowest_apex = None
-#         self.enter_node = None
-#         self.count_of_apexes = 0
-#         self.old_node = None
-#
-#     def get_lowest_apex(self, enter_node=None, count_of_apexes=0, old_node=None):
-#         self.nodes = enter_node
-#         self.count_of_apexes = count_of_apexes
-#         self.old_node = old_node
-#         if enter_node is None:
-#             self.nodes = list(self.dict.keys())
-#         for self.apex in self.nodes:
-#             try:
-#                 self.lowest_apex = self.dict[self.apex]
-#                 # del self.trial_dict[self.apex]
-#                 print(self.lowest_apex)
-#
-#                 # sefl.count_of_apexes_for_tree_apex += 1
-#                 APEXES_WITH_WEIGHTS[self.apex] = self.count_of_apexes
-#                 self.count_of_apexes += 1
-#                 self.get_lowest_apex(self.lowest_apex, self.count_of_apexes, self.old_node)
-#                 # self.count_of_apexes_for_tree_apex = 0
-#             except Exception as ex:
-#                 # print('Exception -> ',ex)
-#                 # APEXES_WITH_WEIGHTS[self.lowest_apex[0]] = self.count_of_apexes
-#                 print(self.count_of_apexes)
-#
-#         self.count_of_apexes = 0
+class Apex:
+    def __init__(self, dictionary):
+        self.dict = dictionary
+        # self.trial_dict = self.dict.copy()
+        self.nodes = None
+        self.lowest_apex = None
+        self.enter_node = None
+        self.count_of_apexes = 0
+        self.old_node = None
+
+    def get_lowest_apex(self, enter_node=None, count_of_apexes=0, old_node=None):
+        self.nodes = enter_node
+        self.count_of_apexes = count_of_apexes
+        self.old_node = old_node
+        if enter_node is None:
+            self.nodes = list(self.dict.keys())
+        for self.apex in self.nodes:
+            try:
+                self.lowest_apex = self.dict[self.apex]
+                # del self.trial_dict[self.apex]
+                print(self.lowest_apex)
+
+                # sefl.count_of_apexes_for_tree_apex += 1
+                APEXES_WITH_WEIGHTS[self.apex] = self.count_of_apexes
+                self.count_of_apexes += 1
+                self.get_lowest_apex(self.lowest_apex, self.count_of_apexes, self.old_node)
+                # self.count_of_apexes_for_tree_apex = 0
+            except Exception as ex:
+                # print('Exception -> ',ex)
+                # APEXES_WITH_WEIGHTS[self.lowest_apex[0]] = self.count_of_apexes
+                print(self.count_of_apexes)
+
+        self.count_of_apexes = 0
 
 
 for apex in GRAPH:
