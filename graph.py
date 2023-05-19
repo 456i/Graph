a998680e6a86858d5f04d4bc765d9ea302104da7
456i <timofeymarushko@gmail.com>
2023-05-19 21:46:49 +0300

    Some files were deleted, structure of graph*construction.py was remade into OOP.

--- graph.py
+++ graph.py
@@ -1,82 +1,58 @@
 GRAPH = [[1, 2], [1, 3], [1, 4], [1, 5], [2, 6], [3, 7], [3, 8], [3, 9], [3, 10], [4, 11], [4, 12], [5, 13], [5, 14], [5, 15],
          [6, 17], [8, 18], [10, 19], [10, 20], [12, 21], [12, 22], [13, 23], [15, 24], [15, 25], [18, 26], [18, 27], [19, 28],
-         [20, 29], [20, 30], [22, 31], [22, 31], [23, 33], [25, 34], [25, 35], ]
+         [20, 29], [20, 30], [22, 31], [22, 31], [23, 33], [25, 34], [25, 35]]
 COUNT_OF_APEX_new_tree = 15
-COUNT_NODES = len(GRAPH)-1
-APEXES_WITH_WEIGHTS = {apex: None for apex in range(1, COUNT_NODES)}
+APEXES_WITH_WEIGHTS = {apex: 1 for apex in range(1, 36)}
 print(APEXES_WITH_WEIGHTS, 'this is apex with weights')
 STACK = {}
-from graph_construction import GraphAnimator
 
 
-class Node:
-    def __init__(self, start_node, end_node):
-        self.start_node = start_node
-        self.end_node = end_node
-        self.all_apexes = list(STACK.keys())
-        if self.start_node in self.all_apexes:
-            all_apex = STACK[self.start_node]
-            all_apex.append(self.end_node)
+def fill_STACK():
+    global STACK
+    for start_node, end_node in GRAPH:
+        all_apexes = list(STACK.keys())
+        if start_node in all_apexes:
+            all_apex = STACK[start_node]
+            all_apex.append(end_node)
         else:
-            STACK[self.start_node] = {}
-            STACK[self.start_node] = [self.end_node]
+            STACK[start_node] = {}
+            STACK[start_node] = [end_node]
 
 
-def make_node():
-    global STACK, APEXES_WITH_WEIGHTS
-    STACK = {apex: len(apex_adjacent) for apex, apex_adjacent in STACK.items()}
-    all_apexes = list(STACK.keys())
-    all_apexes__ = list(APEXES_WITH_WEIGHTS.keys())
-    print(all_apexes__, all_apexes)
-    for apex in all_apexes:
-        if apex in all_apexes__:
-            all_apexes__.remove(apex)
-
-    for apex in all_apexes__:
-        STACK[apex] = 1
+def descript_count_of_apexes_in_apex():
+    global APEXES_WITH_WEIGHTS
+    for apex in list(STACK.keys()):
+        APEXES_WITH_WEIGHTS[apex] = len(list(STACK[apex]))
 
 
-class Apex:
-    def __init__(self, dictionary):
-        self.dict = dictionary
-        self.list_of_nodes = list(self.dict.keys())
-        self.nodes = None
-        self.lowest_apex = None
-        self.enter_node = None
-        self.count_of_apexes = 0
-        self.old_node = None
-        self.old_apex = 0
-
-    def get_lowest_apex(self, enter_node=None, old_node=None, count_of_apexes=0):
-        self.nodes = enter_node
-        self.old_node = old_node
-        self.count_of_apexes = count_of_apexes
-        if enter_node is None:
-            self.nodes = list(self.dict.keys())
-        for self.apex in self.nodes:
-            if self.apex in self.list_of_nodes:
-                if self.old_apex < self.apex:
-                    self.old_apex = self.apex
+def dfs(start_apex, apex, count_of_containing_apexes, STACK):
+    global finished_dict__, list_
+    if apex in list(STACK.keys()):
+        for node in STACK[apex]:
+            list_.append(node)
+            count_of_containing_apexes += APEXES_WITH_WEIGHTS[node]
+            dfs(start_apex, node, count_of_containing_apexes, STACK)
+    else:
+        try:
+            finished_dict__[start_apex] = count_of_containing_apexes
+        except:
+            finished_dict__ = {start_apex: count_of_containing_apexes}
 
 
-                print(f'1line old_apex into if {self.old_apex}\napex -> {self.apex}\n count of apexes {self.count_of_apexes}')
-                self.lowest_apex = self.dict[self.apex]
-                self.count_of_apexes += 1
-                print(f'2line old_apex into if {self.old_apex}\napex -> {self.apex}\n count of apexes {self.count_of_apexes}')
-                self.get_lowest_apex(self.lowest_apex, self.old_node, self.count_of_apexes)
+def define_count_of_apex(dict):
+    global finished_dict__, list_
+    dict = dict.copy()
+    dict.pop(1)
+    list_ = []
+    for apex, count_of_containing_apexes in dict.items():
+        list_.append(apex)
+        dfs(apex, apex, count_of_containing_apexes, STACK)
 
-        APEXES_WITH_WEIGHTS[self.old_apex] = self.count_of_apexes
-        self.old_apex = 0
-        self.count_of_apexes = 0
-        print(f'old_apex into after init {self.old_apex}')
+    print(finished_dict__, 'finished dict\n', list_, 'list')
 
 
-for apex in GRAPH:
-    Node(apex[0], apex[1])
-
+fill_STACK()
+descript_count_of_apexes_in_apex()
+define_count_of_apex(APEXES_WITH_WEIGHTS)
+print(APEXES_WITH_WEIGHTS, ' APEXES_WITH_WEIGHTS')
 print(STACK)
-apex = Apex(STACK)
-apex.get_lowest_apex()
-
-print(STACK)
-print(APEXES_WITH_WEIGHTS)
--- test1.py
+++ test1.py
@@ -13,7 +13,7 @@ class Apex:
         self.pos = None
         self.fig = None
         self.ax = None
-        self.visited_apexes = []
+        self.visited_apexes = [1, 1]
         self.create_graph()
 
     def create_graph(self):
@@ -22,7 +22,7 @@ class Apex:
         self.G.add_edges_from(self.graph)
 
     def change_color(self, node, color):
-        node_colors = {n: 'blue' for n in self.G.nodes}
+        node_colors = {n: 'lightblue' for n in self.G.nodes}
         node_colors[node] = color
         return node_colors
 
@@ -30,15 +30,13 @@ class Apex:
         self.nodes = enter_node
         self.old_node = old_node
         self.count_of_apexes = count_of_apexes
-        if enter_node is None:
-            self.nodes = list(self.dict.keys())
+        self.nodes = list(self.dict.keys()) if enter_node is None else self.nodes
         for self.apex in self.nodes:
+            self.visited_apexes.append(self.apex)
             if self.apex in self.list_of_nodes:
                 self.old_node = self.apex
                 if self.old_node < self.apex:
                     self.old_node = self.apex
-                print(self.apex)
-                self.visited_apexes.append(self.apex)
                 self.lowest_apex = self.dict[self.apex]
                 self.count_of_apexes += 1
                 self.get_lowest_apex(self.lowest_apex, self.old_node, self.count_of_apexes)
@@ -46,8 +44,8 @@ class Apex:
 
     def draw_graph(self, node, ax):
         ax.clear()
-        node_colors = self.change_color(node, 'red')
-        nx.draw(self.G, self.pos, with_labels=True, node_color=list(node_colors.values()), ax=ax)
+        node_colors = self.change_color(node, 'pink')
+        nx.draw_kamada_kawai(self.G, node_color=list(node_colors.values()), with_labels=True)
 
 
 class GraphAnimator:
@@ -73,7 +71,7 @@ class GraphAnimator:
 
             self.apex.draw_graph(self.node_to_update[self.i], self.ax)
 
-        self.animation = FuncAnimation(self.fig, update_frame, frames=None, interval=1000, repeat=False)
+        self.animation = FuncAnimation(self.fig, update_frame, cache_frame_data=False, interval=1000, repeat=False)
 
         def on_key_press(event):
             if event.key == ' ':
@@ -87,17 +85,16 @@ class GraphAnimator:
         self.fig.canvas.mpl_connect('key_press_event', on_key_press)
         plt.show()
 
+
 # Пример использования
 STACK = {
     1: [2, 3, 4, 5], 2: [6], 3: [7, 8, 9, 10], 4: [11, 12], 5: [13, 14, 15], 6: [17], 8: [18], 10: [19, 20], 12: [21, 22], 13: [23], 15: [24, 25],
-18: [26, 27], 19: [28], 20: [29, 30], 22: [31, 31], 23: [33], 25: [34, 35]
+    18: [26, 27], 19: [28], 20: [29, 30], 22: [31, 31], 23: [33], 25: [34, 35]
 }
 
-graph = [
-[1, 2], [1, 3], [1, 4], [1, 5], [2, 6], [3, 7], [3, 8], [3, 9], [3, 10], [4, 11], [4, 12], [5, 13], [5, 14], [5, 15],
-[6, 17], [8, 18], [10, 19], [10, 20], [12, 21], [12, 22], [13, 23], [15, 24], [15, 25], [18, 26], [18, 27], [19, 28],
-[20, 29], [20, 30], [22, 31], [22, 31], [23, 33], [25, 34], [25, 35]
-]
+graph = [[1, 2], [1, 3], [1, 4], [1, 5], [2, 6], [3, 7], [3, 8], [3, 9], [3, 10], [4, 11], [4, 12], [5, 13], [5, 14], [5, 15],
+         [6, 17], [8, 18], [10, 19], [10, 20], [12, 21], [12, 22], [13, 23], [15, 24], [15, 25], [18, 26], [18, 27], [19, 28],
+         [20, 29], [20, 30], [22, 31], [22, 31], [23, 33], [25, 34], [25, 35]]
 
 animator = GraphAnimator(graph)
 animator.start_animation()
