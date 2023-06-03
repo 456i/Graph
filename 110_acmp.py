18a2cb3ff8d5d6b96e3499c0fa1b696189df54a0
456i <timofeymarushko@gmail.com>
2023-06-03 13:57:58 +0300

    Fully remade code

--- 110_acmp.py
+++ 110_acmp.py
@@ -18,21 +18,25 @@ def generate_beautiful_numbers(num_1, num_2, delta=1):
                 buffer_str += str(num_2)
             buffer_int = num_1 - int(buffer_str)
             result += buffer_str + ' + '
+            buffer_str = ''
             generate_beautiful_numbers(buffer_int, num_2)
             out = True
 
         elif num_2 == int(digit) and not out:
             buffer_str += str(num_2)
-            if num_2 - num_1 == 0:
-                result += buffer_str
 
-        elif num_2 > int(digit) and not out:
-            delta += 1
-            power = 10 ^ delta
-            generate_beautiful_numbers(buffer_int, num_2/power, delta)
+        if num_2 > int(digit) and not out:
+            for i in range(len(str(num_1)[str(num_1).index(digit) + 1:])):
+                buffer_str += str(num_2)
+                print(buffer_str)
+            buffer_int = num_1 - int(buffer_str)
+            result += buffer_str + ' + '
+            buffer_str = ''
+            generate_beautiful_numbers(buffer_int, num_2)
             out = True
+
+    result += buffer_str
 
 
 generate_beautiful_numbers(number_1, number_2, 1)
 print(result)
-
